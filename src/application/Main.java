d1d8ba9bd066dd311862158227359f2b2958f70b
agentAlaska2 <agentalaska2@gmail.com>
2023-04-27 09:14:10 -0500

    Put tiled floor into eclipse with tiledGrid

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,9 +1,11 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.TilePane;
 import javafx.stage.Stage;
 
 
@@ -11,10 +13,24 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			GridPane mapGrid = new GridPane();
 			
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			Image background = new Image("C:\\Users\\McMahonJake\\eclipse-workspace\\ProgrammingAssignment3\\src\\application\\ImageAssets\\TempFloor.png", 30, 30, false, false);	
+			
+			TilePane tiledBackground = new TilePane();
+			
+			
+			for(int i = 0; i < 225; i++) {
+				tiledBackground.getChildren().add(new ImageView(background));
+			}
+			tiledBackground.setPrefColumns(15);
+			
+			tiledBackground.setPrefRows(15);
+			tiledBackground.setAlignment(Pos.CENTER);
+			
+			
+			
+			
+			Scene scene = new Scene(tiledBackground,450,450);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
